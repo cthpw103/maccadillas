af2c837e1d9b02148aa50267d43caf4bb6103349
t3nt <44480063+prometheus1337@users.noreply.github.com>
2019-09-03 22:55:42 +0100

    i dont know gui programming
    send help

---
+++ main.py
@@ -0,0 +1,36 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+import geocoder
+from nearest import get
+import requests
+import beautifulsoup4
+class lol(Gtk.Window):
+def __init__(self):
+# thingy for buttons
+lol = Gtk.Box(spacing=6)
+self.add(lol) # AND THIS TOO
+# is it bad i got that from the example aaaaaa idk how to gui programming
+switcht = Gtk.Button.new_with_label("Nearest McDonald's") # ok i had to look at example aaaaaa
+switcht.connect("clicked", self.nearest) # frick idk how to gui so yeah i had to l ook at example and thanks example
+# get app
+maccas = requests.get("https://www.mcdonalds.com/us/en-us.html")
+# get request to the mcdonalds website
+headline = maccas.find(id="headline-text") # yes thanks so
+# get headlines using beautifulsoup
+description = maccas.find(id="headline-description")
+# and the thing for the asterisks
+asterisk = maccas.find(id="legal-text")
+# check if we find headline
+if headline
+xd = Gtk.Label()
+showheadline = xd.setmarkup("<big>" + headline + "</big>")
+showdescription = xd.set_text(description)
+showthingsidkwhy = xd.set_title(asterisk)
+# if not, do the thing
+else:
+xd = Gtk.Label()
+xd.set_text("Could not retrieve headline!")
+def nearest():
+nearest.get()
+# todo
