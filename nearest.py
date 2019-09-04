905ccf7a02e0b907340372707dd22905524da32b
cth103 <cthdev@email.com>
2019-09-04 23:29:12 +0100

    ugly

--- nearest.py
+++ nearest.py
@@ -5,11 +5,11 @@ import json
 def get():
 # send req
 	haha = requests.get('https://ipinfo.io/loc')
-rofl = requests.get("https://ipinfo.io")
-lul = json.loads(rofl) # forgot this existed
-lol = haha.text
-reg = lul.lul["country"]
-xd = lol.list(lol.split(','))
+	rofl = requests.get("https://ipinfo.io/json")
+	lul = json.loads(rofl) # forgot this existed
+	lol = haha.text
+	reg = lul.lul["country"]
+	xd = lol.list(lol.split(','))
 # https://www.mcdonalds.com/googleapps/GoogleRestaurantLocAction.do?method=searchLocation
 # todo: do something wiith this 
 # ill probably do it tmw :) 
