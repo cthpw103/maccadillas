c3f03ecca06d2a1d5e25b29e7039465792d579fa
t3nt <44480063+prometheus1337@users.noreply.github.com>
2019-09-04 23:25:34 +0100

    still not completed

--- nearest.py
+++ nearest.py
@@ -6,7 +6,9 @@ def get():
 # send req
 	haha = requests.get('https://ipinfo.io/loc')
 rofl = requests.get("https://ipinfo.io")
+lul = json.loads(rofl) # forgot this existed
 lol = haha.text
+reg = lul.lul["country"]
 xd = lol.list(lol.split(','))
 # https://www.mcdonalds.com/googleapps/GoogleRestaurantLocAction.do?method=searchLocation
 # todo: do something wiith this 
